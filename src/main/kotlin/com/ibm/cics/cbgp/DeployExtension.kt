386708f9b8d0f91576f9fec671333d61e1bfe8ab
Stewart Francis <stewartfrancis@uk.ibm.com>
2019-09-19 10:13:06 +0100

    Hybrid kotlin implementation support
    Signed-off-by: Stewart Francis <stewartfrancis@uk.ibm.com>

---
+++ src/main/kotlin/com/ibm/cics/cbgp/DeployExtension.kt
@@ -0,0 +1,11 @@
+package com.ibm.cics.cbgp
+
+open class DeployExtension {
+    var cicsplex = ""
+    var region = ""
+    var bunddef = ""
+    var csdgroup = ""
+    var url = ""
+    var username = ""
+    var password = ""
+}
