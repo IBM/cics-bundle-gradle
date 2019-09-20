9a7f3678ab398c52c6c108f89814891f7ac11f72
Steve Bolton <boltonst@uk.ibm.com>
2019-09-20 12:00:24 +0100

    Remove commented out code
    Signed-off-by: Steve Bolton <boltonst@uk.ibm.com>

--- src/main/groovy/com/ibm/cics/cbgp/AbstractBundleTask.groovy
+++ src/main/groovy/com/ibm/cics/cbgp/AbstractBundleTask.groovy
@@ -29,77 +29,9 @@ import java.util.stream.Collectors
 
 abstract class AbstractBundleTask extends DefaultTask {
 
-
-//
-//	public static final String BAD_VERSION_NUMBER = 'Unable to parse project version number'
-//	String defaultjvmserver
-
 	protected BundlePublisher initBundlePublisher() throws GradleException {
-//		println("OutputDirectory is: ${outputDirectory.toString()}")
-//
-//		VersionNumber versionNumber = getProjectVersionNumber()
-//		File f = outputDirectory.getAsFile().get()
-//
-//
-//		BundlePublisher bundlePublisher = new BundlePublisher(
-//				f.toPath(),
-//				project.getArtifactId(),
-//				versionNumber.getMajor(),
-//				versionNumber.getMinor(),
-//				versionNumber.getPatch(),
-//				versionNumber.getQualifier()
-//		)
-//		addStaticBundleResources(bundlePublisher)
-//		return bundlePublisher
+		// TODO
 	}
 
-	private void addStaticBundleResources(BundlePublisher bundlePublisher) throws GradleException {
-		//Add bundle parts for any resources
-//		Path basePath = baseDir.toPath()
-//		Path bundlePartSource = basePath.resolve("src/main/resources")
-//		getLog().info("Gathering bundle parts from " + basePath.relativize(bundlePartSource))
-//
-//		if (Files.exists(bundlePartSource)) {
-//			if (Files.isDirectory(bundlePartSource)) {
-//				try {
-//					List<Path> paths = Files
-//							.walk(bundlePartSource)
-//							.filter(Files::isRegularFile)
-//							.collect(Collectors.toList())
-//
-//					for (Path toAdd : paths) {
-//						try {
-//							bundlePublisher.addStaticResource(
-//									bundlePartSource.relativize(toAdd),
-//									() -> Files.newInputStream(toAdd)
-//							)
-//						} catch (BundlePublisher.PublishException e) {
-//							throw new GradleException("Failure adding static resource " + toAdd + ": " + e.getMessage(), e)
-//						}
-//					}
-//				} catch (IOException e) {
-//					throw new GradleException("Failure adding static resources", e)
-//				}
-//			} else {
-//				throw new GradleException("Static bundle resources directory " + bundlePartSource + "wasn't a directory")
-//			}
-//		} else {
-//			//Ignore if it doesn't exist
-//		}
-	}
-
-
-//	private VersionNumber getProjectVersionNumber() throws GradleException {
-//		def pv = project.version
-//		if (!pv instanceof String) {
-//			throw new GradleException(BAD_VERSION_NUMBER)
-//		} else {
-//			VersionNumber versionNumber = VersionNumber.parse((String) pv)
-//			if (versionNumber == VersionNumber.UNKNOWN) {
-//				throw new GradleException(BAD_VERSION_NUMBER)
-//			}
-//			return versionNumber
-//		}
-//	}
 
 }
