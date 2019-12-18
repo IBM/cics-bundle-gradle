4dc5e315d31c3d8b2e88348a049f56afe810b89d
Ben Cox <1038350+ind1go@users.noreply.github.com>
2019-12-18 22:23:37 +0000

    Use system properties rather than env vars for project variables
    Signed-off-by: Ben Cox <1038350+ind1go@users.noreply.github.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,12 +3,13 @@ set -x
 
 # If this is a master build then publish
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  ORG_GRADLE_PROJECT_signing.keyId="$GPG_KEY_NAME" \
-  ORG_GRADLE_PROJECT_signing.password="$GPG_PASSPHRASE" \
-  ORG_GRADLE_PROJECT_signing.secretKeyRingFile="$GPG_SECRET_KEYRING_FILE" \
-  ORG_GRADLE_PROJECT_ossrhUser="$OSSRH_USERNAME" \
-  ORG_GRADLE_PROJECT_ossrhPassword="$OSSRH_PASSWORD" \
-   ./gradlew build publishAll
+  ./gradlew \
+  -Dorg.gradle.project.signing.keyId="$GPG_KEY_NAME" \
+  -Dorg.gradle.project.signing.password="$GPG_PASSPHRASE" \
+  -Dorg.gradle.project.signing.secretKeyRingFile="$GPG_SECRET_KEYRING_FILE" \
+  -Dorg.gradle.project.ossrhUser="$OSSRH_USERNAME" \
+  -Dorg.gradle.project.ossrhPassword="$OSSRH_PASSWORD" \
+   build publishAll
 # Otherwise just build and test
 else
   ./gradlew build
