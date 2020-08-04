8ca7cfd220557c0db7d1a9ea41836752577bcff2
Ledina Hido-Evans <ledina.hido@uk.ibm.com>
2020-08-04 13:19:35 +0100

    Prep for release 1.0.1
    Signed-off-by: Ledina Hido-Evans <ledina.hido@uk.ibm.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "com.ibm.cics"
-version = "1.0.1-SNAPSHOT"
+version = "1.0.1"
 val isReleaseVersion by extra(!version.toString().endsWith("SNAPSHOT"))
 
 gradlePlugin {
@@ -84,7 +84,7 @@ repositories {
 defaultTasks("build")
 
 dependencies {
-    implementation("com.ibm.cics:cics-bundle-common:1.0.1")
+    implementation("com.ibm.cics:cics-bundle-common:1.0.2")
     testImplementation("junit:junit:4.12")
     testImplementation("com.github.tomakehurst:wiremock-jre8:2.25.1")
     testImplementation("org.spockframework:spock-core:1.1-groovy-2.4") {
