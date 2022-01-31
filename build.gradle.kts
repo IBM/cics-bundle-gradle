8e0871c04eb734cdf53602b695aafef654041af3
Ledina Hido-Evans <ledina.hido@uk.ibm.com>
2022-01-31 19:37:11 +0000

    Update to latest dependencies
    This includes moving to CICS TS 5.6
    
    Signed-off-by: Ledina Hido-Evans <ledina.hido@uk.ibm.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("groovy")
     id("java-gradle-plugin")
     id("maven-publish")
-    id("com.gradle.plugin-publish") version "0.12.0"
+    id("com.gradle.plugin-publish") version "0.20.0"
     id("signing")
     `kotlin-dsl`
 }
@@ -90,8 +90,8 @@ defaultTasks("build")
 dependencies {
     implementation("com.ibm.cics:cics-bundle-common:1.0.2")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.31.0")
-    testImplementation(enforcedPlatform("org.spockframework:spock-bom:2.0-groovy-3.0"))
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.32.0")
+    testImplementation(enforcedPlatform("org.spockframework:spock-bom:2.1-M2-groovy-3.0"))
     testImplementation("org.spockframework:spock-junit4")
     testImplementation("org.spockframework:spock-core")
 }
