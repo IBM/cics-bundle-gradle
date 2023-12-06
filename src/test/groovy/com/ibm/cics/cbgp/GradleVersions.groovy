2f1ddda90f7aa0435ff0a8d0a984f418ac6ecce1
Ben Cox <1038350+ind1go@users.noreply.github.com>
2023-12-06 19:21:49 +0000

    Up top testing level to Gradle 8.5
    Signed-off-by: Ben Cox <1038350+ind1go@users.noreply.github.com>

--- src/test/groovy/com/ibm/cics/cbgp/GradleVersions.groovy
+++ src/test/groovy/com/ibm/cics/cbgp/GradleVersions.groovy
@@ -2,7 +2,7 @@ package com.ibm.cics.cbgp
 
 abstract class GradleVersions {
 
-    static List GRADLE_VERSIONS = ["7.6.1", "8.3"]
+    static List GRADLE_VERSIONS = ["7.6.1", "8.5"]
 
     static List onAllVersions(List inputs) {
         [GRADLE_VERSIONS, inputs]
